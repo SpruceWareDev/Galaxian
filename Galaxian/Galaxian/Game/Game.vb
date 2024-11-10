6e465560fb13cdc4488a9dec2404977a445da30d
SpruceWareDev <69159094+SpruceWareDev@users.noreply.github.com>
2024-11-10 22:13:52 +0000

    Start Project

---
+++ Galaxian/Galaxian/Game/Game.vb
@@ -0,0 +1,16 @@
+﻿Public Class Game
+
+    Private _form As Form1
+
+    Public Sub New(parent As Form)
+        _form = parent
+    End Sub
+
+    Public Sub Initialize()
+        ' Init engine components
+    End Sub
+
+    Public Sub Run()
+        _form.Show()
+    End Sub
+End Class
---
+++ Galaxian/Galaxian/Game/Global/Graphics/Renderer.vb
@@ -0,0 +1,10 @@
+﻿Public Class Renderer
+
+    Private _pictureBox As PictureBox
+    Private _bitmap As Bitmap
+    Private _graphics As Graphics
+
+    Public Function Initialize(parent As Form1) As PictureBox
+        Return _pictureBox
+    End Function
+End Class
